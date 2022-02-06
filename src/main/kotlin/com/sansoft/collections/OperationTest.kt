5722a677badbdabdcf68b0f9f9988d89f73b293f
Thiago S. Melo <thiagosantiagomelo@gmail.com>
2022-02-06 16:46:40 -0300

    Update OperationTest to use count, find, any

--- src/main/kotlin/com/sansoft/collections/OperationTest.kt
+++ src/main/kotlin/com/sansoft/collections/OperationTest.kt
@@ -17,4 +17,16 @@ fun main() {
 
     val salaryLargerThem2500 = salary.filter { it > 2500 }
     salaryLargerThem2500.forEach { println(it) }
+
+    println("****************************")
+    println(salary.count {it in 1499.0..5000.0})
+
+    println("****************************")
+    println(salary.find {it == 2000.0})
+    println(salary.find {it == 500.0})
+
+    println("****************************")
+    println(salary.any {it == 2000.0})
+    println(salary.any {it == 500.0})
+
 }
